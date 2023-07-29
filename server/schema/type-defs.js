97afb5e06fb7f3f89e2d32eda9441fb6c7103e41
daksh <daksh44jain@gmail.com>
2023-07-29 13:13:17 +0530

    added resolvers

---
+++ server/schema/type-defs.js
@@ -0,0 +1,58 @@
+// const { gql } = require('apollo-server')
+
+// const typeDefs = gql`
+// type User {
+//   id: ID!
+//   name: Stirng! // ! --> required
+//   username: Stirng!
+//   age: int!
+//   nationality: Stirng!
+// }
+
+// type Query {
+//   users: [User!]!
+// }
+// `
+
+// module.exports = {
+//   typeDefs
+// }
+
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    age: Int!
+    nationality: Nationality
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
+    UKRAINE
+  }
+`
+
+module.exports = { typeDefs }
---
+++ server/server.js
@@ -0,0 +1,21 @@
+// const { ApolloServer } = require('@apollo/server')
+
+// const { typeDefs } = require('./schema/type-defs')
+// const { resolvers } = require('./schema/resolvers')
+// const server = new ApolloServer({ typeDefs, resolvers })
+
+// // const url = 3000
+
+// server.start().then(() => {
+//   console.log(`server is running at}`)
+// })
+
+const { ApolloServer } = require('apollo-server')
+const { typeDefs } = require('./schema/type-defs')
+const { resolvers } = require('./schema/resolvers')
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`YOUR API IS RUNNING AT: ${url} :)`)
+})
